2bf29a828b1a8da4da04dac9314b643ca5d8207e
Lei-Lei-alpha <Lei.Lei2@nottingham.ac.uk>
2026-04-19 00:53:24 +0100

    deleted:    .ipynb_checkpoints/anisotropy-checkpoint.py 	deleted:    .ipynb_checkpoints/atoms-checkpoint.py 	deleted:    .ipynb_checkpoints/c2_at_dict-checkpoint.py 	deleted:    .ipynb_checkpoints/data-checkpoint.py 	deleted:    .ipynb_checkpoints/df3-checkpoint.py 	deleted:    .ipynb_checkpoints/hbond-checkpoint.py 	deleted:    .ipynb_checkpoints/msd-checkpoint.py 	deleted:    .ipynb_checkpoints/sel_tools-checkpoint.py 	deleted:    .ipynb_checkpoints/traj-checkpoint.py 	deleted:    .ipynb_checkpoints/trj-checkpoint.py 	deleted:    .ipynb_checkpoints/vis-checkpoint.py 	deleted:    fishmol/.ipynb_checkpoints/dimer-checkpoint.py

--- .ipynb_checkpoints/vis-checkpoint.py
+++
@@ -1,6 +0,0 @@
-from ase import Atoms as ase_Atoms
-from ase.visualize import view as ase_view
-
-def view(atoms, viewer = "ngl"):
-    atoms = ase_Atoms(symbols = atoms.symbs, positions = atoms.pos, cell = atoms.cell.lattice)
-    ase_view(atoms)
